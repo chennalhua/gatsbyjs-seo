679846c52fa7293793ff28d1e0bcabf7d251581a
Naihua Chen <naihua.chen@mail.goldennet.com.tw>
2023-04-25 10:00:51 +0800

    [Update -- Added react-snap]

--- src/index.js
+++ src/index.js
@@ -9,20 +9,20 @@ import './assets/stylesheet/all.scss';
 
 import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <AppRouter />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AppRouter />
+  </React.StrictMode>
+);
 
-const rootElement = document.getElementById("root");
+// const rootElement = document.getElementById("root");
 
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrate(<AppRouter />, rootElement);
-} else {
-  ReactDOM.render(<AppRouter />, rootElement);
-}
+// if (rootElement.hasChildNodes()) {
+//   ReactDOM.hydrate(<AppRouter />, rootElement);
+// } else {
+//   ReactDOM.render(<AppRouter />, rootElement);
+// }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
